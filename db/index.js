0280b13598dc09b79610a247fd05d1feacf56160
nikhil jain <nikhiljain74095@gmail.com>
2024-09-28 17:51:23 +0530

    10 hours compelete

--- db/index.js
+++ db/index.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants";
-
+import express from 'express';
+const app = express();
  const connectDB = async()=>{
     try{
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
          console.log(`\n mongodb connection !! DB HOST: ${connectionInstance.connection.host}`)
+        
     }
     catch(error){
       console.log("MongoDb connection error",error);
